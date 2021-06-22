15ff031fa73e84a673781ca06dcd3e1c9c1a7731
Rahul Dahal <rdaahal@gmail.com>
2021-06-22 14:16:45 +0545

    fix:style add padding-y around nav wrap

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -3,8 +3,10 @@ import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <nav className="">
-      <Link href="/">Logo here</Link>
+    <nav className="w-screen">
+      <div className="container py-4">
+        <Link href="/">Logo here</Link>
+      </div>
     </nav>
   );
 }
