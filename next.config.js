ffd3f9c5ef5b58836da5fcf3008d661c508065fc
Rahul Dahal <rdaahal@gmail.com>
2021-06-22 14:15:38 +0545

    chore: support raw svg imports

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// bootstrapped from https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+
+module.exports = {
+  webpack: (config, options) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: '@svgr/webpack'
+    });
+
+    // Important: return the modified config
+    return config;
+  },
+  images: {
+    disableStaticImages: true // refer: https://stackoverflow.com/a/68012194
+  }
+};
