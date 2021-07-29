5947fc6ced5ba2da7ef7d1d7b7c6fa4560475071
Rahul Dahal <rdaahal@gmail.com>
2021-07-29 15:30:40 +0545

    refactor: add index file to eaevery component

---
+++ components/Landing/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from './Hero';
+import Features from './Features';
+import Community from './Community';
+import About from './About';
+
+export default function Landing() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <About />
+      <Community />
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
+import Landing from '@components/Landing';
 import Layout from '@components/Layout';
-import Hero from '@components/Landing/Hero';
-import Features from '@components/Landing/Features';
-import Community from '@components/Landing/Community';
-import About from '@components/Landing/About';
 
 export default function Home() {
   return (
     <Layout title="Ask Buddie Bot">
-      <Hero />
-      <Features />
-      <About />
-      <Community />
+      <Landing />
     </Layout>
   );
 }
