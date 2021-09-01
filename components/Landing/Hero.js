1846d6d54b1628e1585050a95f8a44ce6b2e33c1
Rahul Dahal <rdaahal@gmail.com>
2021-09-01 17:08:16 +0545

    wip: making landing>hero mobile friendly

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -6,10 +6,10 @@ import Heading from '@components/Heading';
 
 export default function Hero() {
   return (
-    <main className="relative">
-      <div className="container relative heroContainer flex items-center justify-between">
-        <div>
-          <Heading modifier="mb-6">
+    <main className="relative px-2">
+      <div className="container relative heroContainer flex flex-col-reverse md:flex-row items-end md:items-center justify-between">
+        <div className="relative z-20 -mt-20 md:mt-0">
+          <Heading modifier="mb-6 pt-4 bg-gradient-to-r from-white-faded-1 to-white-faded-9 md:pt-0 lg:bg-none">
             Ask Buddie&apos;s
             <span className="text-brand-400 mt-4 font-bold block">
               Bot Buddy !
@@ -21,12 +21,16 @@ export default function Hero() {
           </p>
           <Button modifier="mt-12 relative pointer">Get Sexy!</Button>
         </div>
-        <div>
-          <img src="/imgs/logo-full.png" alt="" className="relative z-10" />
-          <Ellipses className="absolute w-1/2 bottom-0 right-0" />
+        <div className="relative w-72 md:w-1/2 md:static">
+          <img
+            src="/imgs/logo-full.png"
+            alt=""
+            className="absolute bottom-0 w-72 md:w-full z-10 md:relative"
+          />
+          <Ellipses className="w-72 md:absolute md:w-1/2 bottom-0 right-0" />
         </div>
       </div>
-      <Wave className="max-h-screen-sm absolute -z-1 -bottom-16 left-0" />
+      <Wave className="max-h-screen-sm absolute -z-1 -bottom-32 md:-bottom-16 left-0" />
     </main>
   );
 }
