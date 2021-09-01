32fd40976af47de5f83a601d9091c82cdd3adbc4
Rahul Dahal <rdaahal@gmail.com>
2021-09-01 08:27:52 +0545

    fix: mobile design now scales to fit the viewport

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,6 +8,7 @@ function index({ title, children }) {
     <>
       <Head>
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Nav />
       {children}
