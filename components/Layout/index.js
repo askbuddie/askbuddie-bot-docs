91e5f57080da55b9bac6dcdab110b7685be94acf
Rahul Dahal <rdaahal@gmail.com>
2021-09-15 11:27:41 +0545

    refactor: append 'AskBuddie Bot' at the end of title

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -7,7 +7,7 @@ function index({ title, children }) {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{`${title} | AskBuddie Bot`}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Nav />
