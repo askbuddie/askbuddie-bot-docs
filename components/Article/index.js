90b4e9b49f959c7c021b41cbe7ca34029b668bcc
Rahul Dahal <rdaahal@gmail.com>
2021-10-10 08:39:52 +0545

    refactor: use custom components to render MD

--- components/Article/index.js
+++ components/Article/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import Heading from '@components/Heading';
 
 export default function Article({ title, content }) {
   return (
     <article>
-      <h1>{title}</h1>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <Heading level="2">{title}</Heading>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter language={match[1]} PreTag="div" {...props}>
+                {children}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+          h1({ level, children }) {
+            return <Heading level={level + 1}>{children}</Heading>;
+          }
+        }}
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </ReactMarkdown>
     </article>
   );
 }
--- docs/how-to-this/Handling-events-react.md
+++ docs/how-to-this/Handling-events-react.md
@@ -1,3 +1,5 @@
+# Hello
+
 Handling events with React elements is very similar to handling events on DOM elements. There are some syntax differences:
 
 * React events are named using camelCase, rather than lowercase.
